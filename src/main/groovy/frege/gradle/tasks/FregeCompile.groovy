36d1edfe9e68fef8310c0d2bcf98e571d04c014f
Joseph Price <pricejosephd@gmail.com>
2022-02-21 09:35:34 -0500

    try destination dir

--- src/main/groovy/frege/gradle/tasks/FregeCompile.groovy
+++ src/main/groovy/frege/gradle/tasks/FregeCompile.groovy
@@ -6,7 +6,7 @@ import org.gradle.api.file.Directory
 import org.gradle.api.file.FileCollection
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFiles
-import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.compile.AbstractCompile
@@ -99,7 +99,7 @@ class FregeCompile extends AbstractCompile {
     @Optional @InputFiles
     FileCollection fregepath
 
-    @InputDirectory
+    @OutputDirectory
     File destinationDir
 
     @Input
