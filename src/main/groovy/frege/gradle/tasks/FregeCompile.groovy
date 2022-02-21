642c6c1fabf7ab8bf2c2b17e58c346ad04773e18
Joseph Price <pricejosephd@gmail.com>
2022-02-21 09:33:50 -0500

    update file annotation

--- src/main/groovy/frege/gradle/tasks/FregeCompile.groovy
+++ src/main/groovy/frege/gradle/tasks/FregeCompile.groovy
@@ -6,6 +6,7 @@ import org.gradle.api.file.Directory
 import org.gradle.api.file.FileCollection
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFiles
+import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.Optional
 import org.gradle.api.tasks.TaskAction
 import org.gradle.api.tasks.compile.AbstractCompile
@@ -98,7 +99,7 @@ class FregeCompile extends AbstractCompile {
     @Optional @InputFiles
     FileCollection fregepath
 
-    @Input
+    @InputDirectory
     File destinationDir
 
     @Input
